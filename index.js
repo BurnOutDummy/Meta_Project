d1c35777b58ff8eb215d4895746c1e32fa9a0c00
Nischal <nickandel123@gmail.com>
2024-11-09 23:46:21 +0000

    m

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const { Console } = require("console");
 const express = require("express");
+const http = require('http');
+const { console } = require("inspector");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 3000;
+const {Server} = require('socket.io')
+const server = http.createServer(app);
+console.log("hello world ")
 
 app.get('/Home',(req,res)=>{
   res.send("Hello World")
 })
-
-app.listen(port,()=>{
-  console.log(`Server is listening on port : ${port}`)
+const io = new Server(server);
+io.on('connection',()=>{
+  Console.log(`Somebody has connected to the server `)
 })
+
+server.listen(port,()=>{
+  console.log(`Server is listening on port : ${port}`)
+});
+
