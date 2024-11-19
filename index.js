3988a4ccd09af820797f4c3eb1e37e051161b0df
Nischal <nickandel123@gmail.com>
2024-11-19 09:19:41 +0000

    nothing important

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {Server} = require('socket.io')
 const port = process.env.PORT || 5000
 const httproutes = require('./routes/route')
 const pagenotfound = require('./errors/pagenotfound')
+const { console } = require('inspector')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,12 +13,10 @@ const io = new Server(server)
 // Http server 
 app.use(pagenotfound)
 app.use('/home',httproutes)
-
-io.on('connection',()=>{
+io.on('connection',(client)=>{
   console.log("Idiot login")
-})
 
-
-server.listen(port,()=>{
-  console.log(`Server is listening on port: ${port}`)
+  client.on('chat message',(message)=>{
+    console.log('message: ',message)
+  })
 })
